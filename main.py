242214c1c209f62ffb36a40894384b68799ab280
Vitali Bekarevich <vbekarevich1980@gmail.com>
2022-07-15 14:18:01 +0300

    Add stopping the splash container and rename the output file
    If the script crashes the Splash container doesn't stop and
    when rerunning the script it shows the docker exception.
    Try-except 'docker stop' was added to prevent that.
    
    The name of the file with date-time conflicts with Windows.
    The output file was renamed.

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from pathlib import Path
 from openpyxl.drawing.image import Image
 import openpyxl
 
-from twisted.internet import defer # reactor,
-from scrapy.crawler import CrawlerRunner, CrawlerProcess
+from scrapy.crawler import CrawlerProcess
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from stock_market.spiders.stock_market_spider import StockMarketSpider
@@ -20,22 +19,16 @@ if __name__ == "__main__":
 
     #os.system('docker pull scrapinghub/splash')
     #time.sleep(10)
+    try:
+        os.system('docker stop splash')
+    except:
+        pass
+    time.sleep(15)
     os.system('docker run -d -p 8050:8050 --rm --name splash scrapinghub/splash')
-    time.sleep(10)
+    time.sleep(5)
 
     configure_logging()
     settings = get_project_settings()
-    # runner = CrawlerRunner(settings)
-    #
-    # @defer.inlineCallbacks
-    # def crawl():
-    #     yield runner.crawl(ProxySpider)
-    #     #yield runner.crawl(BroswaySpider)
-    #     reactor.stop()
-    #
-    # crawl()
-    # reactor.run()
-
 
     install_reactor('twisted.internet.asyncioreactor.AsyncioSelectorReactor')
     process = CrawlerProcess(settings=settings)
@@ -72,7 +65,8 @@ if __name__ == "__main__":
             img.height = 357
             destination_sheet.add_image(img, f'{column_id}{src_row[0].row}')
 
-    destination_wb_obj.save(f'Stock Market {datetime.datetime.today()}.xlsx')
+    #destination_wb_obj.save(f'Stock Market {datetime.datetime.today()}.xlsx')
+    destination_wb_obj.save('stock_market_output.xlsx')
     destination_wb_obj.close()
 
     os.system('docker stop splash')
